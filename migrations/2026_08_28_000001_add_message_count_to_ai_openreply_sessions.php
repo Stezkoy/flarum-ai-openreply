9e48af619debe3fe1591d2c28378194c89260f19
Stezkoy <stezko@gmail.com>
2026-08-28 21:05:40 +0500

    fix: use integer+unsigned column type instead of unsignedInteger for MariaDB grammar

--- migrations/2026_08_28_000001_add_message_count_to_ai_openreply_sessions.php
+++ migrations/2026_08_28_000001_add_message_count_to_ai_openreply_sessions.php
@@ -3,5 +3,5 @@
 use Flarum\Database\Migration;
 
 return Migration::addColumns('ai_openreply_sessions', [
-    'message_count' => ['unsignedInteger', 'default' => 0],
+    'message_count' => ['integer', 'unsigned' => true, 'default' => 0],
 ]);
