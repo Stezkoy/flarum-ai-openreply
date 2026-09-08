f78612d3c34fcf33a2ca2ee97dfd0049d5b589df
stezkoy <stezko@gmail.com>
2026-09-08 23:19:25 +0500

    style: unify admin layout

--- js/src/admin/components/AIOpenReplySettingsPage.js
+++ js/src/admin/components/AIOpenReplySettingsPage.js
@@ -27,43 +27,72 @@ export default class AIOpenReplySettingsPage extends ExtensionPage {
             m('p', app.translator.trans(PREFIX + '.admin.settings.opencode_intro')),
             m('a.AIOpenReplyLink', { href: 'https://opencode.ai/', target: '_blank', rel: 'noopener' }, 'https://opencode.ai/'),
           ]),
-          this._group('opencode_url_label', 'opencode_url_help', 'input', 'opencode_url', {
-            placeholder: 'http://localhost:4096',
-          }),
-          this._group('opencode_username_label', 'opencode_username_help', 'input', 'opencode_username', {
-            placeholder: 'opencode',
-          }),
-          this._group('opencode_password_label', 'opencode_password_help', 'input', 'opencode_password', {
-            type: 'password',
-          }),
-          this._agentGroup(),
-          this._group('opencode_system_prompt_label', 'opencode_system_prompt_help', 'textarea', 'opencode_system_prompt', {
-            rows: 3,
-            placeholder: app.translator.trans(PREFIX + '.admin.settings.opencode_system_prompt_placeholder'),
-          }),
-          this._modelGroup(),
-          this._actionsGroup(),
-          this._group('user_prompt_label', 'user_prompt_help', 'input', 'user_prompt', {
-            type: 'number',
-            required: true,
-          }),
-          this._group('user_prompt_badge_label', 'user_prompt_badge_help', 'input', 'user_prompt_badge_text'),
-          this._switchGroup(),
-
-          m('.Form-group', [
-            m('label', app.translator.trans(PREFIX + '.admin.settings.limits_label')),
+          this._section('connection_section', [
+            this._group('opencode_url_label', 'opencode_url_help', 'input', 'opencode_url', {
+              placeholder: 'http://localhost:4096',
+            }),
+            this._group('opencode_username_label', 'opencode_username_help', 'input', 'opencode_username', {
+              placeholder: 'opencode',
+            }),
+            this._group('opencode_password_label', 'opencode_password_help', 'input', 'opencode_password', {
+              type: 'password',
+            }),
+            this._agentGroup(),
+          ]),
+          this._section('model_section', [
+            this._group('opencode_system_prompt_label', 'opencode_system_prompt_help', 'textarea', 'opencode_system_prompt', {
+              rows: 3,
+              placeholder: app.translator.trans(PREFIX + '.admin.settings.opencode_system_prompt_placeholder'),
+            }),
+            this._modelGroup(),
+          ]),
+          this._section('trigger_section', [
+            this._group('user_prompt_label', 'user_prompt_help', 'input', 'user_prompt', {
+              type: 'number',
+              required: true,
+            }),
+            this._group('user_prompt_badge_label', 'user_prompt_badge_help', 'input', 'user_prompt_badge_text'),
+            this._switchGroup(),
+            this._tagsGroup(),
+          ]),
+          this._section('limits_label', [
             this._numberGroup('max_active_sessions_label', 'max_active_sessions_help', 'max_active_sessions'),
             this._numberGroup('max_messages_per_session_label', 'max_messages_per_session_help', 'max_messages_per_session'),
             this._numberGroup('session_ttl_days_label', 'session_ttl_days_help', 'session_ttl_days'),
           ]),
-
-          m('.Form-group', [
-            m('label', app.translator.trans(PREFIX + '.admin.settings.retry_label')),
+          this._section('retry_label', [
             this._numberGroup('retry_attempts_label', 'retry_attempts_help', 'retry_attempts'),
             this._numberGroup('retry_delay_seconds_label', 'retry_delay_seconds_help', 'retry_delay_seconds'),
           ]),
-
-          this._tagsGroup(),
+          this._section('actions_label', [
+            m('.ButtonGroup', [
+              Button.component(
+                {
+                  className: 'Button',
+                  loading: this.loadingHealth,
+                  onclick: () => this.checkConnection(),
+                },
+                app.translator.trans(PREFIX + '.admin.settings.check_connection_label')
+              ),
+              Button.component(
+                {
+                  className: 'Button',
+                  loading: this.loadingCount,
+                  onclick: () => this.checkSessionCount(),
+                },
+                app.translator.trans(PREFIX + '.admin.settings.session_count_label')
+              ),
+              Button.component(
+                {
+                  className: 'Button Button--danger',
+                  loading: this.loadingCloseAll,
+                  onclick: () => this.closeAll(),
+                },
+                app.translator.trans(PREFIX + '.admin.settings.close_all_sessions_label')
+              ),
+            ]),
+            m('p.helpText', this.statusMessage || app.translator.trans(PREFIX + '.admin.settings.actions_help')),
+          ]),
 
           m('.Form-group.Form-controls', this.submitButton()),
         ]),
@@ -71,36 +100,10 @@ export default class AIOpenReplySettingsPage extends ExtensionPage {
     );
   }
 
-  _actionsGroup() {
-    return m('.Form-group', [
-      m('label', app.translator.trans(PREFIX + '.admin.settings.actions_label')),
-      m('.ButtonGroup', [
-        Button.component(
-          {
-            className: 'Button',
-            loading: this.loadingHealth,
-            onclick: () => this.checkConnection(),
-          },
-          app.translator.trans(PREFIX + '.admin.settings.check_connection_label')
-        ),
-        Button.component(
-          {
-            className: 'Button',
-            loading: this.loadingCount,
-            onclick: () => this.checkSessionCount(),
-          },
-          app.translator.trans(PREFIX + '.admin.settings.session_count_label')
-        ),
-        Button.component(
-          {
-            className: 'Button Button--danger',
-            loading: this.loadingCloseAll,
-            onclick: () => this.closeAll(),
-          },
-          app.translator.trans(PREFIX + '.admin.settings.close_all_sessions_label')
-        ),
-      ]),
-      m('p.helpText', this.statusMessage || app.translator.trans(PREFIX + '.admin.settings.actions_help')),
+  _section(titleKey, children) {
+    return m('.AIOpenReplySettings-section', [
+      m('h3', app.translator.trans(PREFIX + '.admin.settings.' + titleKey)),
+      m('.AIOpenReplySettings-sectionBody', children),
     ]);
   }
 
